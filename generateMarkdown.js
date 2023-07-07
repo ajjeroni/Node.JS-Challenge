f76d29df8875f5db2ca4694caf3bba3ad6b8f7bb
Adan <adanjeronimo213@gmail.com>
2023-07-06 22:44:13 -0700

    copied code

---
+++ generateMarkdown.js
@@ -0,0 +1,43 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+ 
+  return `
+# ${answers.Title}
+
+## Description
+${answers.Description}
+
+## Table of Contents
+${answers['Table of Contents']}
+
+## Installation Instructions
+${answers.Installation}
+
+## Usage Information
+${answers.Usage}
+
+## Contribution Guidelines
+${answers.Contributing}
+
+## Test Instructions
+${answers.Tests}
+
+## License
+This project is licensed under the ${answers.License} license.
+`;
+}
+
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,84 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require('./generateMarkdown');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter Project Title:',
+            name: 'Title',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid Project Title is required.");
+                }
+                return true;
+            }            
+        },
+        {
+            type: 'input',
+            message: 'Enter Description:',
+            name: 'Description',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid Description is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter Table of Contents:',
+            name: 'Table of Contents',
+        },
+        {
+            type: 'input',
+            message: 'Enter Installation Instructions:',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'Enter Usage Information:',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'Enter Contribution Guidelines:',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message: 'Enter Test Instructions:',
+            name: 'Tests',
+        },
+        {
+            type: 'list',
+            message: 'Choose a License:',
+            name: 'License',
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        }
+    ])
+    .then((answers) => {
+        writeReadMeFile(answers);
+    });
+
+    function writeReadMeFile(answers) {
+        // Convert the answers object to a string
+        const readmeContent = generateMarkdown(answers);
+      
+        // Write the content to a README.md file
+        fs.writeFile('README.md', readmeContent, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log('README.md generated successfully!');
+        });
+      }
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init(); 
